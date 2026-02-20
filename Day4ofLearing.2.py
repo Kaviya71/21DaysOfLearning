da453ac6eebff10802cb0c5cd6f99f05244c32f9
Kaviya71 <kaviyakarthi71@gmail.com>
2026-02-20 08:20:11 +0530

    Add files via upload

---
+++ Day4ofLearing.2.py
@@ -0,0 +1,13 @@
+a=[]
+print('enter 7 numbers')
+for i in range(1,8):
+    num=int(input('enter num' + str(i)))
+    a.append(num)
+print(a)
+sum=0
+for i in a:
+    sum+=i
+print(sum)
+            
+            
+            
---
+++ Day4ofLearning3.py
@@ -0,0 +1,4 @@
+print(" To print the cube of numbers")
+n=int(input("enter the number:"))
+for i in range(1,n+1):
+    print("number is :" +str(i)+"and cube of  "+str(i)+" is :"+str(i*i*i))
