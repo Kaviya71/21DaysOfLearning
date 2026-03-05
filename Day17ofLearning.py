56bf6b77d106b8c9f489d8aa88560b62301cbe90
Kaviya71 <kaviyakarthi71@gmail.com>
2026-03-05 21:13:57 +0530

    Add files via upload

---
+++ Day17ofLearning.py
@@ -0,0 +1,30 @@
+class laptop:
+    price=0
+    processor=""
+    RAM=""
+hp=laptop()
+dell=laptop()
+lenova=laptop()
+
+hp.price=50000
+hp.processor="i5"
+hp.RAM="8GB"
+dell.price=60000
+dell.processor="i7"
+dell.RAM="16GB"
+lenova.price=40000
+lenova.processor="i3"
+lenova.RAM="6GB"
+print("The price of the HP laptop is",hp.price)
+print("The processor of HP laptop is",hp.processor)
+print("The RAM of HP laptop is",hp.RAM)
+print("The price of the Dell laptop is",dell.price)
+print("The processor of dell laptop is",dell.processor)
+print("The RAM of dell laptop is",dell.RAM)
+print("The price of the Lenova laptop is",lenova.price)
+print("The processor of enova laptop is",lenova.processor)
+print("The RAM of Lenova laptop is",lenova.RAM)
+
+hp_price
+dell_price
+    
